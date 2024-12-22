951bffa3128a232438b02e383dbdfaaf5b929930
Ankit Kumar <jangidankit722@gmail.com>
2024-12-22 17:01:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+let line = document.getElementById('line');
+let angle = 0;
+
+let rotateline = () =>{
+	line.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+            angle += 2; 
+            if (angle >= 360) {
+                angle = 0; 
+            }
+            requestAnimationFrame(rotateLine); 
+        };
+
+        rotateLine(); 
+}
+
+
+	
